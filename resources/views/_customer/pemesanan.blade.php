77ba4f9d354fa6752d154671bac1343290685acb
rafifandy <rafifmochammad@gmail.com>
2022-06-30 16:18:00 +0700

    Pemesanan customer

--- resources/views/_customer/pemesanan.blade.php
+++ resources/views/_customer/pemesanan.blade.php
@@ -24,6 +24,12 @@
             <h1>Pemesanan</h1>
         <br/>
         <br/>
+        <hr/>
+        @if (session('status'))
+              <div class="alert alert-success">
+                  {{ session('status') }}
+              </div>
+        @endif
 
         <?php $countu = 0 ?>
         @foreach($pelanggan as $u)
@@ -56,7 +62,48 @@
                     <button type="submit" class="btn btn-success" onclick="return confirm('Mulai Pemesanan?')">Mulai Pemesanan</button>
                 </form>
             @elseif($countpms == 1)
-                Pilih barang
+            @foreach($penjualan as $p)
+                @if($sp->id_pelanggan == $uid and $p->pemesanan == 1)
+                    <?php 
+                        $d_id0 = str_pad($p->id_penjualan,11,'0',STR_PAD_LEFT);
+                        $d_id = str_pad($d_id0,12,'P',STR_PAD_LEFT);
+                    ?>
+                    <br/>
+                        <h5>ID - {{$d_id}}</h5>
+                    <br/>
+                    <table id="t" class="display cell-border">
+                    <thead>
+                        <tr style="background-color:#BDDFFF">
+                            <th>No</th>
+                            <th>Kode barang</th>
+                            <th>Barang</th>
+                            <th>Jumlah Pesan</th>
+                            <th>Harga satuan</th>
+                            <th>Total Harga</th>
+                            <th>Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php $count = 0 ?>
+                    @foreach($p->barang as $pb)
+                        <tr>
+                            <?php $count = $count+1 ?>
+                            <td>{{$count}}</td>
+                            <td>{{$pb->id_barang}}</td>
+                            <td>{{$pb->nama_barang}}</td>
+                            <td>{{$pb->pivot->jumlah_barang}}</td>
+                            <td>{{$pb->pivot->harga_barang}}</td>
+                            <td>{{$pb->pivot->total_harga_barang}}</td>
+                            <td><button class="badge badge-info" style="width:80px;margin:5px" data-toggle="modal" data-target="#editModal{{$p->id_penjualan}}_{{$pb->id_barang}}">Edit</button></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                    </table>
+                    <hr/>
+                        <h5>Pilih Barang</h5>
+                    <br/>
+                @endif
+            @endforeach
             @endif
             
         @endif  
