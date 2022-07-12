e2d2798f14846454ca0a21a70aa34feef51fa7e6
rafifandy <rafifmochammad@gmail.com>
2022-07-12 08:37:07 +0700

    barang

--- resources/views/barang/barang.blade.php
+++ resources/views/barang/barang.blade.php
@@ -341,17 +341,17 @@
                 reader.readAsDataURL(input.files[0]);
             }
         }
-        function readURL2(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
+    function readURL2(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-                reader.onload = function (a) {
-                    $('#blah2')
-                        .attr('src', a.target.result);
-                };
+            reader.onload = function (a) {
+                $('#blah2')
+                    .attr('src', a.target.result);
+            };
 
-                reader.readAsDataURL(input.files[0]);
-            }
+            reader.readAsDataURL(input.files[0]);
         }
+    }
 </script>
 @endsection
